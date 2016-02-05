a7c1f75766e1c2ce016aa2cc207549de1002a833
Jake Eraklidis <jake.eraklidis@gmail.com>
2016-02-05 02:06:45 -0800

    Create FileProcessor.java

---
+++ FileProcessor.java
@@ -0,0 +1,32 @@
+/*
+    This class will parse all the words in a file (will remove all non word characters) and return them as an ArrayList 
+ */
+package edu.uis.csc478b;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileProcessor 
+{
+    private String fileAsAString;
+    
+    public FileProcessor(String fileAsAString)
+    {
+        this.fileAsAString = fileAsAString;
+    }
+    
+    public ArrayList<String> returnWords()
+    {
+        ArrayList<String> listOfWords = new ArrayList<>();
+        String firstPass = fileAsAString.toLowerCase();
+        String [] strings = firstPass.split("\\.");
+        
+        for(String s : strings)
+        {
+            String [] words = s.replaceAll("[^a-zA-Z\\d\\s]", "").trim().split("\\s");
+            listOfWords.addAll(Arrays.asList(words));
+        }
+        
+        return listOfWords;
+    }
+}
